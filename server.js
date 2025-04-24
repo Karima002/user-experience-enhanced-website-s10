7b7067ac69dbd3e2fbc2d01a7d060dc0de0a7196
Karima002 <karima.mouadi@hva.nl>
2025-04-24 18:03:15 +0200

    #7 Server.js afgerond

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/ar', async function (request, response) {
 app.get ('/object/:id', async function (request, response) {
     const artworkId = request.params.id; 
     const apiResponse = await fetch(`https://fdnd-agency.directus.app/items/fabrique_art_objects/${artworkId}?fields=title,image,summary,artist,location,displayDate,materials,techniques,objectNumber,recordType`)
-    const apiResponseJSON = await apiResponse.json(); 
+    const apiResponseJSON = await apiResponse.json() 
     
     response.render("objects.liquid", { artwork: apiResponseJSON.data })
   })
@@ -47,16 +47,15 @@ app.get('/ar/object/:id', async function (request, response) {
     const apiResponse = await fetch(`https://fdnd-agency.directus.app/items/fabrique_art_objects/${artworkId}?fields=titleAR,image,artist,displayDate,summaryAR,materials,techniques,objectNumber,objectNameAR`)
     const apiResponseJSON = await apiResponse.json() 
     
-    response.render("objectar.liquid", { artwork: apiResponseJSON.data }); 
+    response.render("objectar.liquid", { artwork: apiResponseJSON.data })
   })
 
 
-// Voor de pagina
+// Voor de pagina's met het formulier
 app.get('/acquisition', async function (request, response) {
     const apiResponse = await fetch('https://fdnd-agency.directus.app/items/fabrique_art_objects'
     );
-    const apiResponseJSON = await apiResponse.json(); // Lees van de response van die fetch het JSON object in, waar we iets mee kunnen doen
-    
+    const apiResponseJSON = await apiResponse.json(); 
     // Voor het weergeven van de opgehaalde data in op de acquisitionpagina
     const messageResponse = await fetch("https://fdnd-agency.directus.app/items/fabrique_messages/?filter={%22for%22:%20{%22_contains%22:%20%22Karima_%22}}")
     const messageResponseJSON = await messageResponse.json(); // Lees van de response van die fetch het JSON object in, waar we iets mee kunnen doen
@@ -69,18 +68,77 @@ app.get('/acquisition', async function (request, response) {
   });
   
   
-  app.get('/acquisition/ar', async function (request, response) {
+  app.get('/ar/acquisition', async function (request, response) {
     const apiResponse = await fetch('https://fdnd-agency.directus.app/items/fabrique_art_objects'
     );
-    const apiResponseJSON = await apiResponse.json(); // Lees van de response van die fetch het JSON object in, waar we iets mee kunnen doen
-    
+    const apiResponseJSON = await apiResponse.json()
+
     // Voor het weergeven van de opgehaalde data in op de acquisitionpagina
     const messageResponse = await fetch("https://fdnd-agency.directus.app/items/fabrique_messages/?filter={%22for%22:%20{%22_contains%22:%20%22Karima_%22}}")
-    const messageResponseJSON = await messageResponse.json(); // Lees van de response van die fetch het JSON object in, waar we iets mee kunnen doen
+    const messageResponseJSON = await messageResponse.json()
   
     response.render("acquisitionar.liquid", { 
       api: apiResponseJSON.data, 
       messages: messageResponseJSON.data,
-      id: 'karima-form'
+    })
+  })
+
+  app.get('/succesfull', async function (request, response) {
+    const apiResponse = await fetch('https://fdnd-agency.directus.app/items/fabrique_art_objects')
+    const apiResponseJSON = await apiResponse.json()
+    
+    response.render("succes.liquid", { api: apiResponseJSON.data })
+  })
+
+  app.get('/ar/succesfull', async function (request, response) {
+    const apiResponse = await fetch('https://fdnd-agency.directus.app/items/fabrique_art_objects')
+    const apiResponseJSON = await apiResponse.json()
+    
+    response.render("succesar.liquid", { api: apiResponseJSON.data })
+  })
+
+
+//POST routes
+let forms = [] //array voor het opslaan van formulieren
+
+app.post('/acquisition', async function (request, response) {
+  
+    await fetch("https://fdnd-agency.directus.app/items/fabrique_messages", {
+      method: "POST",
+      body: JSON.stringify({ //gegevens die  naar de server wordt gestuurd, omzetten in een JSON-string.
+        for: "Karima_" + request.body.name,  // De naam van de gebruiker, toegevoegd aan een vaste string "Karima_" voor het alleen weergeven van mijn posts.
+        from: request.body.email,  // E-mail van de gebruiker
+        text: request.body.description,  // De beschrijving die door de gebruiker is ingevoerd in het formulier
+      }),
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8'
+      }, //request met post, met headers geef je aan wat er is meegegeven, je geeft informatie over wat je in de request heb meegegeven. 
     });
-  });
+    
+    response.redirect(303, '/succesfull') //Na het versturen van de gegevens naar de API wordt de gebruiker doorgestuurd naar de pagina /succesfull
+  
+  })
+
+  app.post('/ar/acquisition', async function (request, response) {
+  
+    await fetch("https://fdnd-agency.directus.app/items/fabrique_messages", {
+      method: "POST",
+      body: JSON.stringify({
+        for: "Karima_" + request.body.name,
+        from: request.body.email,
+        text: request.body.description,
+      }),
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8'
+      }, 
+    });
+    
+    response.redirect(303, '/ar/succesfull')
+  })
+
+  app.set('port', process.env.PORT || 8000)
+
+// Start Express op, gebruik daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get('port'), function () {
+  console.log(`Daarna kun je via http://localhost:${app.get('port')}/ jouw interactieve website bekijken.\n\nThe Web is for Everyone. Maak mooie dingen 🙂`)})
+  
